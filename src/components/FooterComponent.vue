26d4d56ed73aedf56a0ec8b870724e7dddf51447
Rigon Osmani <97856794+rigonosmani@users.noreply.github.com>
2023-02-10 01:00:47 +0100

    Add files via upload

---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,27 @@
+<template>
+  <footer>
+    <p>© {{ currentYear }} {{ conf.SITE_NAME }}</p>
+  </footer>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      currentYear: new Date().getFullYear(),
+      conf: {
+        SITE_NAME: "Your Site Name",
+      },
+    };
+  },
+};
+</script>
+
+<style>
+footer {
+  background-color: #333;
+  color: #fff;
+  text-align: center;
+  padding: 20px;
+}
+</style>
